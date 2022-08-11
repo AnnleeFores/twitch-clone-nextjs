b4f834844901d120b776437a9adda4c378bca997
Annlee Fores <annleefores@gmail.com>
2022-08-11 19:02:52 +0530

    complete

---
+++ components/Main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Hero from "./Hero";
+import IconBar from "./IconBar";
+import LiveChannels from "./LiveChannels";
+
+const Main = () => {
+  return (
+    <div className="absolute left-[64px] xl:left-[220px]">
+      <Hero />
+      <LiveChannels />
+      <IconBar />
+    </div>
+  );
+};
+
+export default Main;
---
+++ data/mock_data.js
@@ -0,0 +1,104 @@
+export const rec_channels = [
+  {
+    id: 1,
+    avatar: "https://robohash.org/dictavoluptatemeum.png?size=50x50&set=set1",
+    username: "jmcbryde0",
+    game_name: "Tres-Zap",
+    rank: 38,
+  },
+  {
+    id: 2,
+    avatar: "https://robohash.org/quidoloribusveniam.png?size=50x50&set=set1",
+    username: "fcrosen1",
+    game_name: "Ronstring",
+    rank: 48,
+  },
+  {
+    id: 3,
+    avatar: "https://robohash.org/earumtotamaliquid.png?size=50x50&set=set1",
+    username: "rhonack2",
+    game_name: "Domainer",
+    rank: 25,
+  },
+  {
+    id: 4,
+    avatar: "https://robohash.org/sequiconsequaturquas.png?size=50x50&set=set1",
+    username: "wmcinnes3",
+    game_name: "Stronghold",
+    rank: 23,
+  },
+  {
+    id: 5,
+    avatar: "https://robohash.org/estinventoreporro.png?size=50x50&set=set1",
+    username: "yashleigh4",
+    game_name: "Tres-Zap",
+    rank: 23,
+  },
+  {
+    id: 6,
+    avatar: "https://robohash.org/nemoquisoluta.png?size=50x50&set=set1",
+    username: "ekepling5",
+    game_name: "Latlux",
+    rank: 82,
+  },
+  {
+    id: 7,
+    avatar:
+      "https://robohash.org/doloresvoluptatumquisquam.png?size=50x50&set=set1",
+    username: "bpirkis6",
+    game_name: "Toughjoyfax",
+    rank: 64,
+  },
+  {
+    id: 8,
+    avatar: "https://robohash.org/etiurequi.png?size=50x50&set=set1",
+    username: "wricharz7",
+    game_name: "Subin",
+    rank: 5,
+  },
+  {
+    id: 9,
+    avatar: "https://robohash.org/cumquererumsed.png?size=50x50&set=set1",
+    username: "ivanbrugh8",
+    game_name: "Andalax",
+    rank: 41,
+  },
+  {
+    id: 10,
+    avatar: "https://robohash.org/estdoloremnemo.png?size=50x50&set=set1",
+    username: "hsyson9",
+    game_name: "Zoolab",
+    rank: 60,
+  },
+];
+
+export const top_channels = [
+  {
+    id: 1,
+    avatar: "https://robohash.org/dictavoluptatemeum.png?size=50x50&set=set1",
+    username: "jmcbryde0",
+    game_name: "Tres-Zap",
+    rank: 38,
+  },
+  {
+    id: 2,
+    avatar: "https://robohash.org/quidoloribusveniam.png?size=50x50&set=set1",
+    username: "fcrosen1",
+    game_name: "Ronstring",
+    rank: 48,
+  },
+  {
+    id: 3,
+    avatar: "https://robohash.org/earumtotamaliquid.png?size=50x50&set=set1",
+    username: "rhonack2",
+    game_name: "Domainer",
+    rank: 25,
+  },
+  {
+    id: 4,
+    avatar: "https://robohash.org/sequiconsequaturquas.png?size=50x50&set=set1",
+    username: "wmcinnes3",
+    game_name: "Stronghold",
+    rank: 23,
+  },
+];
